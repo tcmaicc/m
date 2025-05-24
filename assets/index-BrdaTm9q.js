100a64782cba7821c598ecc3fead1895e2b4dc87
tcmaicc <tcmai@protonmail.com>
2025-05-25 00:56:50 +0800

    chore: release v0.0.3

---
+++ assets/index-BrdaTm9q.js
@@ -0,0 +1,1 @@
+import{a as t}from"./function-call-CEHJEL-P.js";import{s as a}from"./index-DB1Y3nx4.js";a(t);
--- assets/index-CtkqSfzS.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./function-call-DYwzhz9n.js";import{s as a}from"./index-DtVU_o5h.js";a(t);
