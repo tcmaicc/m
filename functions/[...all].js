080562fe58ca493eb674e9169feed6c811dc09e3
tcmaicc <tcmai@protonmail.com>
2025-06-03 01:13:20 +0800

    chore: release v0.0.8

--- functions/[...all].js
+++ functions/[...all].js
@@ -1,42 +1,45 @@
 export async function onRequest(context) {
-	const {
-		request
-	} = context;
-	const url = new URL(request.url);
+  const { request } = context;
+  const url = new URL(request.url);
 
-	// 代理到你的真实服务器
-	const targetUrl = `https://api.tcmai.cc${url.pathname}${url.search}`;
+  const targetUrl = `https://api.tcmai.cc${url.pathname}${url.search}`;
 
-	const newHeaders = new Headers(request.headers);
-	newHeaders.delete('host');
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
+        'Access-Control-Allow-Headers': '*',
+      },
+    });
+  }
 
-	let body = null;
-	if (request.method !== 'GET' && request.method !== 'HEAD') {
-		body = await request.arrayBuffer();
-	}
+  const newHeaders = new Headers(request.headers);
+  newHeaders.delete('host');
 
-	const response = await fetch(targetUrl, {
-		method: request.method,
-		headers: newHeaders,
-		body,
-		redirect: 'manual',
-	});
+  let body = null;
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    body = await request.arrayBuffer();
+  }
 
-	const responseHeaders = new Headers(response.headers);
-	responseHeaders.set('Access-Control-Allow-Origin', '*');
-	responseHeaders.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
-	responseHeaders.set('Access-Control-Allow-Headers', '*');
+  const response = await fetch(targetUrl, {
+    method: request.method,
+    headers: newHeaders,
+    body,
+    redirect: 'manual',
+  });
 
-	if (request.method === 'OPTIONS') {
-		return new Response(null, {
-			status: 204,
-			headers: responseHeaders
-		});
-	}
+  const responseHeaders = new Headers(response.headers);
+  responseHeaders.set('Access-Control-Allow-Origin', '*');
+  responseHeaders.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
+  responseHeaders.set('Access-Control-Allow-Headers', '*');
 
-	return new Response(response.body, {
-		status: response.status,
-		statusText: response.statusText,
-		headers: responseHeaders,
-	});
+  const clonedBody = await response.arrayBuffer();
+
+  return new Response(clonedBody, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: responseHeaders,
+  });
 }
