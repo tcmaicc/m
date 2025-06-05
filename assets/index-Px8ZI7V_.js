a52bbd11da103b11b115e0afa92736fa910efbaf
tcmaicc <tcmai@protonmail.com>
2025-06-05 12:12:05 +0800

    chore: release v0.1.0

--- assets/index-Px8ZI7V_.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./function-call-GhtZpRtV.js";import{w as s}from"./index-uUe68mG-.js";s(t);
--- assets/index-legacy-BOAOo1SM.js
+++
@@ -1,1 +0,0 @@
-System.register(["./function-call-legacy-DobBVtye.js","./index-legacy-na1mvBcc.js"],(function(e,t){"use strict";var s,c;return{setters:[e=>{s=e.s},e=>{c=e.w}],execute:function(){c(s)}}}));
